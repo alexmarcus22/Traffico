cf76979b71967229f17e6b0fdae77fa9f44d248e
Alexandru Marcus <alexmarcus20141@gmail.com>
2021-02-27 01:55:18 +0200

    added accordion/collapse component, wip components structure

---
+++ client/src/page/components/collapse.component.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="collapse-component">
+		<button
+			class="btn collapse-button"
+			type="button"
+			data-toggle="collapse"
+			:data-target="'#' + target"
+			aria-expanded="false"
+			:aria-controls="target"
+		>
+			What is a professional traffic permit?
+		</button>
+		<div class="collapse" :id="target">
+			<div class="card card-body">
+				Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+				terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+				labore wes anderson cred nesciunt sapiente ea proident.
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+  namer: "Collapse Component",
+  props: {
+    target: String
+  }
+};
+</script>
+
+<style>
+</style>
